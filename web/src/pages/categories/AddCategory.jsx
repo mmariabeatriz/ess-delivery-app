41dea3cb3cec18c88f0d076fb2409744c957c066
mmariabeatriz <mbcb@CESAR-LDAP.INTRANET>
2023-04-08 02:11:39 -0300

    adding other page items

--- web/src/pages/categories/AddCategory.jsx
+++ web/src/pages/categories/AddCategory.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import { isInputNull } from "../../shared/functions/isInputNull";
 import { isDuplicateCategory } from "../../shared/functions/isDuplicateCategory";
 import PrimaryButton from "../../components/atoms/primary-button/PrimaryButton";
-
+import * as Icon from 'react-bootstrap-icons';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 export default function AddCategory() {
   const [showForm, setShowForm] = React.useState(false);
   const [newCategory, setNewCategory] = React.useState("");
   const [warningMessage, setWarningMessage] = React.useState(null);
   const [currentCategories, setCurrentCategories] = React.useState([]);
+  const url = window.location.href
 
   React.useEffect(() => {
     fetch("http://localhost:3001/categories")
@@ -58,10 +60,19 @@ export default function AddCategory() {
   }
 
   return (
-    <div>
-      <h1>Categories</h1>
-      <PrimaryButton buttonContent='Adicionar categoria'  onClick={handleAddCategoryClick} data-testid="add-category-button"></PrimaryButton>
-      {showForm && (
+    <div className='restaurant-menu-root'>
+      <div className='header'>
+        <div className='title-description'>
+          <h1>Cardápio</h1>
+          <p>Gerencie por essa página todos os itens e categorias cadastrados no seu restaurante.</p>
+        </div>
+        <CopyToClipboard className='copy-button' text={url}>
+          <button><Icon.ShareFill color='red' className='share-link'/></button>
+        </CopyToClipboard>
+      </div>
+      <div className='add-category-area'>
+        <PrimaryButton buttonContent='Adicionar categoria'  onClick={handleAddCategoryClick} data-testid="add-category-button"></PrimaryButton>
+        {showForm && (
         <form onSubmit={handleConfirmClick}>
           <label>
             Category name:
@@ -77,6 +88,27 @@ export default function AddCategory() {
         </form>
       )}
       {warningMessage && <p>{warningMessage}</p>}
+      </div>
     </div>
+    // <div>
+    //   <h1>Categories</h1>
+    //   <PrimaryButton buttonContent='Adicionar categoria'  onClick={handleAddCategoryClick} data-testid="add-category-button"></PrimaryButton>
+    //   {showForm && (
+    //     <form onSubmit={handleConfirmClick}>
+    //       <label>
+    //         Category name:
+    //         <input
+    //           type="text"
+    //           value={newCategory}
+    //           onChange={handleNewCategoryChange}
+    //           data-testid="add-category-input"
+    //         />
+    //       </label>
+    //       <PrimaryButton buttonContent='Cancelar' type="button" onClick={handleCancelClick} data-testid="cancel-button"></PrimaryButton>
+    //       <PrimaryButton buttonContent='Confirmar categoria' type="submit" data-testid="create-category-button"></PrimaryButton>
+    //     </form>
+    //   )}
+    //   {warningMessage && <p>{warningMessage}</p>}
+    // </div>
   );
 }
